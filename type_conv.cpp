05b210e014a19c095bf5a524211caa393828ede2
Ahtesham Ul Haq(@hptouchpad519) <hptouchpad519@gmail.com>
2017-02-08 22:41:52 +0500

    Update type_conv.cpp

--- type_conv.cpp
+++ type_conv.cpp
@@ -7,7 +7,7 @@ using namespace std;
 int main()
 {
 int count = 7 ;
-float avgWeight = 155.5E;
+float avgWeight = 155.5F;
 double totalWeight = count * avgWeight ;
 cout << "totalWeight =  " << totalWeight << endl;
 
